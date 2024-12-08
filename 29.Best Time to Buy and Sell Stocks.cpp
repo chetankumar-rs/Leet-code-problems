84e5b36b2c24cf0fa11dda5015c968c450e0c136
Chetankumar R S <chetan5654c@gmail.com>
2024-12-08 20:22:50 +0530

    Update 29.Best Time to Buy and Sell Stocks.cpp

--- 29.Best Time to Buy and Sell Stocks.cpp
+++ 29.Best Time to Buy and Sell Stocks.cpp
@@ -1,15 +1,3 @@
-/* int n  = prices.size();
-        int profit = 0;
-        for(int i = 0;i < n-1;i++){
-            for(int j = i+1;j < n;j++){
-            if(prices[i] < prices[j]){
-            int res = prices[j] - prices[i];
-            profit = max(profit,res);        
-            }
-            }
-        }
-       return profit;*/
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
