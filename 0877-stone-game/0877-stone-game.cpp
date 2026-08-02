77ba5e01339188abb96e56f1900416ce0d0df1ca
Chetankumar R S <1si23cs036@sit.ac.in>
2026-08-02 18:58:58 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.75%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        // int n = piles.size();
-        // int res = 0;
-        // for(int i = 0;i < n;i++)
-        // {
-        //     res ^= piles[i];
-        // }
+        int n = piles.size();
+        int res = 0;
+        for(int i = 0;i < n;i++)
+        {
+            res ^= piles[i];
+        }
 
-        // return res;
-        return true;
+        return res;
+        // return true;
     }
 };
